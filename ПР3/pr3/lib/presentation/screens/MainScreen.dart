165f5b9f5c0c980a61ec4616682efc91286d02d9
Danils-sd <hlebushek.sd@gmail.com>
2024-12-27 00:12:50 +0300

    3

---
+++ ПР3/pr3/lib/presentation/screens/MainScreen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task3/data/StaticData.dart';
+import 'package:flutter_task3/presentation/screens/ProductDetailsScreen.dart';
+import 'package:flutter_task3/presentation/widgets/ProductWidget.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (BuildContext context, int index) {
+          var product = products[index];
+          return ProductWidget(
+            product: product,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProductDetailScreen(product: product),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
